a24183d29b03b327df7535e1a897bd6b0582674c
LancerRevX <50010225+LancerRevX@users.noreply.github.com>
2021-04-22 12:53:44 +0700

    add sorting function

--- container.h
+++ container.h
@@ -19,6 +19,7 @@ struct Container {
 
 void container_init(Container*);
 void container_read(Container*, FILE*);
+void container_sort(Container*);
 void container_write(Container*, FILE*);
 void container_clear(Container*);
 
